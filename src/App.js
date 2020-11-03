18e057ca21289a3f34b1365d8604247770a64206
NikhilReddyP <66748874+NikhilReddyP@users.noreply.github.com>
2020-11-02 23:48:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import HomePage from './pages/homepage/homepage.component'
+import './App.css';
+import {Switch,Route,Redirect } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+
+import { connect } from 'react-redux';
+import { setCurrentUser} from './redux/user/user.action';
+import {selectCurrentUser} from './redux/user/user.selectors'
+import {createStructuredSelector} from 'reselect';
+
+
+
+class App extends React.Component {
+
+
+  unsubscribeFromAuth=null
+
+  componentDidMount(){
+    const {setCurrentUser}=this.props;
+   this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=>{
+      //this.setState({currentUser:user});
+      if(userAuth){
+        const userRef=await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot =>{
+          setCurrentUser({
+            
+              id:snapShot.id,
+              ...snapShot.data()
+      
+          });
+          
+
+        });
+        
+      }
+
+      setCurrentUser(userAuth);
+      
+
+
+      //console.log(user);
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+
+  }
+  
+  
+  render(){
+    return (
+      <div >
+        <Header /> 
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
+          <Route exact path='/signin' render={()=>this.props.currentUser?(<Redirect to='/'/>):<SignInAndSignUpPage/>}/>
+        </Switch>
+      </div>
+    );
+
+  }
+
+}
+
+const mapStateToProps = createStructuredSelector({
+  currentUser:selectCurrentUser
+});
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
